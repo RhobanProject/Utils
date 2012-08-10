e153100e738a8279a369c358f9f2aceea13da80b
Gregwar <g.passault@gmail.com>
2012-08-10 12:02:06 +0200

    [log] Changing log format

--- log.h
+++ log.h
@@ -7,31 +7,34 @@
 #include <string.h>
 #include <time.h>
 
-#ifdef NDEBUG
+#ifndef NDEBUG
 #define LOG_DEBUG 1
 #else
 #define LOG_DEBUG 0
 #endif
 
 #define LOG_CPP(condition, component, message) \
-    ostringstream oss__; \
-    oss__ << message; \
-    LOG(condition, component, "%s", oss__.str().c_str())
+    { \
+        ostringstream oss__; \
+        oss__ << message; \
+        LOG(condition, component, "%s", oss__.str().c_str()); \
+    }
 
 #define LOG(condition, component, ...) \
     if (condition) { \
-        printf("[%s]", component); \
-        if (LOG_DEBUG) { \
-        printf("[%s:%d]", (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__), __LINE__); \
-        } \
-        \
         struct tm now__; \
         time_t timestamp__; \
         \
         time(&timestamp__); \
         now__ = *localtime(&timestamp__); \
-        printf(" [%02d/%02d/%04d %02d:%02d:%02d] ",  \
+        printf("[%02d/%02d/%04d %02d:%02d:%02d] ",  \
                 now__.tm_mday, now__.tm_mon+1, now__.tm_year+100, now__.tm_hour, now__.tm_min, now__.tm_sec); \
+        \
+        if (LOG_DEBUG) { \
+        printf("[%s:%d] ", (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__), __LINE__); \
+        } \
+        \
+        printf("[%s] ", component); \
         \
         printf(__VA_ARGS__); \
         printf("\n"); \
