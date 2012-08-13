674490e4572ae29e7630cdc0a9bf2bf06765354a
Gregwar <g.passault@gmail.com>
2012-08-13 16:56:42 +0200

    License

--- test/test-logging.cpp
+++ test/test-logging.cpp
@@ -1,3 +1,12 @@
+/*************************************************
+ * Publicly released by Rhoban System, August 2012
+ *             www.rhoban-system.fr
+ *
+ * Freely usable for non-commercial purposes
+ *
+ * Licence Creative Commons *CC BY-NC-SA
+ * http://creativecommons.org/licenses/by-nc-sa/3.0
+ *************************************************/
 #include <iostream>
 #include <logging/log.h>
 
