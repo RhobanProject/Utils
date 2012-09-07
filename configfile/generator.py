2a42ffa00890e59fb30f587700ced9cc01d15c97
Gregwar <g.passault@gmail.com>
2012-09-07 23:55:28 +0200

    [configfile] Fix

--- configfile/generator.py
+++ configfile/generator.py
@@ -98,7 +98,7 @@ class Generator(object):
             matches = re.match(r"(.*)(Parameter\()(.+?)(\))(.*)", comment, re.MULTILINE|re.DOTALL)
 
             if matches != None:
-                data = matches.groups()[2].split(',')
+                data = matches.groups()[2].split(',', 2)
                 data = map(lambda x: x.strip(), data)
                 self.properties += [Parameter(self.entry, data[0], property['name'], property['type'], data[1], data[2])]
 
