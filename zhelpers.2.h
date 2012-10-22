c82e7f3cb34815320a84721b44cbfbf0e5961484
Olivier Ly <ly.olivier@gmail.com>
2012-10-23 01:33:02 +0200

    bug fix about delta_t

--- zhelpers.2.h
+++ zhelpers.2.h
@@ -1,14 +1,7 @@
-/*  =====================================================================
-    zhelpers.h
-
-    Helper header file for example applications.
-    =====================================================================
-*/
-
 #ifndef __ZHELPERS_H_INCLUDED__
 #define __ZHELPERS_H_INCLUDED__
 
-//  Include a bunch of headers that we will need in the examples
+// Include a bunch of headers that we will need in the examples
 
 #include <zmq.h>
 
@@ -23,40 +16,29 @@
 #include <assert.h>
 #include <signal.h>
 
-//  Version checking, and patch up missing constants to match 2.1
+// Version checking, and patch up missing constants to match 2.1
 #if ZMQ_VERSION_MAJOR == 2
-#   if ZMQ_VERSION_MINOR == 0
-#       error "Please upgrade to ZeroMQ/2.1 stable for these examples"
-#   endif
-#elif ZMQ_VERSION_MAJOR == 3
-#   error "Please stick with ZeroMQ/2.1 stable for these examples"
-#endif
-#ifndef ZMQ_ROUTER
-#   define ZMQ_ROUTER ZMQ_ROUTER
-#endif
-#ifndef ZMQ_DEALER
-#   define ZMQ_DEALER ZMQ_DEALER
+# error "Please upgrade to ZeroMQ/3.2 for these examples"
 #endif
 
-
-//  Provide random number from 0..(num-1)
+// Provide random number from 0..(num-1)
 #if (defined (__WINDOWS__))
-#   define randof(num)  (int) ((float) (num) * rand () / (RAND_MAX + 1.0))
+# define randof(num) (int) ((float) (num) * rand () / (RAND_MAX + 1.0))
 #else
-#   define randof(num)  (int) ((float) (num) * random () / (RAND_MAX + 1.0))
+# define randof(num) (int) ((float) (num) * random () / (RAND_MAX + 1.0))
 #endif
 
 
-//  Receive 0MQ string from socket and convert into C string
-//  Caller must free returned string. Returns NULL if the context
-//  is being terminated.
+// Receive 0MQ string from socket and convert into C string
+// Caller must free returned string. Returns NULL if the context
+// is being terminated.
 static char *
 s_recv (void *socket) {
     zmq_msg_t message;
     zmq_msg_init (&message);
-    if (zmq_recv (socket, &message, 0))
-        return (NULL);
-    int size = zmq_msg_size (&message);
+    int size = zmq_msg_recv (&message, socket, 0);
+    if (size == -1)
+        return NULL;
     char *string = (char *)malloc (size + 1);
     memcpy (string, zmq_msg_data (&message), size);
     zmq_msg_close (&message);
@@ -64,50 +46,47 @@ s_recv (void *socket) {
     return (string);
 }
 
-//  Convert C string to 0MQ string and send to socket
+// Convert C string to 0MQ string and send to socket
 static int
 s_send (void *socket, char *string) {
-    int rc;
     zmq_msg_t message;
     zmq_msg_init_size (&message, strlen (string));
     memcpy (zmq_msg_data (&message), string, strlen (string));
-    rc = zmq_send (socket, &message, 0);
+    int size = zmq_msg_send (&message, socket, 0);
     zmq_msg_close (&message);
-    return (rc);
+    return (size);
 }
 
-//  Sends string as 0MQ string, as multipart non-terminal
+// Sends string as 0MQ string, as multipart non-terminal
 static int
 s_sendmore (void *socket, char *string) {
-    int rc;
     zmq_msg_t message;
     zmq_msg_init_size (&message, strlen (string));
     memcpy (zmq_msg_data (&message), string, strlen (string));
-    rc = zmq_send (socket, &message, ZMQ_SNDMORE);
+    int size = zmq_msg_send (&message, socket, ZMQ_SNDMORE);
     zmq_msg_close (&message);
-    return (rc);
+    return (size);
 }
 
-//  Receives all message parts from socket, prints neatly
+// Receives all message parts from socket, prints neatly
 //
 static void
 s_dump (void *socket)
 {
     puts ("----------------------------------------");
     while (1) {
-        //  Process all parts of the message
+        // Process all parts of the message
         zmq_msg_t message;
         zmq_msg_init (&message);
-        zmq_recv (socket, &message, 0);
+        int size = zmq_msg_recv (&message, socket, 0);
 
-        //  Dump the message as text or binary
+        // Dump the message as text or binary
         char *data = (char *)zmq_msg_data (&message);
-        int size = zmq_msg_size (&message);
         int is_text = 1;
         int char_nbr;
         for (char_nbr = 0; char_nbr < size; char_nbr++)
             if ((unsigned char) data [char_nbr] < 32
-            ||  (unsigned char) data [char_nbr] > 127)
+            || (unsigned char) data [char_nbr] > 127)
                 is_text = 0;
 
         printf ("[%03d] ", size);
@@ -119,16 +98,16 @@ s_dump (void *socket)
         }
         printf ("\n");
 
-        int64_t more;           //  Multipart detection
+        int64_t more; // Multipart detection
         size_t more_size = sizeof (more);
         zmq_getsockopt (socket, ZMQ_RCVMORE, &more, &more_size);
         zmq_msg_close (&message);
         if (!more)
-            break;      //  Last message part
+            break; // Last message part
     }
 }
 
-//  Set simple random printable identity on socket
+// Set simple random printable identity on socket
 //
 static void
 s_set_id (void *socket)
@@ -139,7 +118,7 @@ s_set_id (void *socket)
 }
 
 
-//  Sleep for a number of milliseconds
+// Sleep for a number of milliseconds
 static void
 s_sleep (int msecs)
 {
@@ -147,13 +126,13 @@ s_sleep (int msecs)
     Sleep (msecs);
 #else
     struct timespec t;
-    t.tv_sec  =  msecs / 1000;
+    t.tv_sec = msecs / 1000;
     t.tv_nsec = (msecs % 1000) * 1000000;
     nanosleep (&t, NULL);
 #endif
 }
 
-//  Return current system clock as milliseconds
+// Return current system clock as milliseconds
 static int64_t
 s_clock (void)
 {
@@ -168,8 +147,8 @@ s_clock (void)
 #endif
 }
 
-//  Print formatted string to stdout, prefixed by date/time and
-//  terminated with a newline.
+// Print formatted string to stdout, prefixed by date/time and
+// terminated with a newline.
 
 static void
 s_console (const char *format, ...)
@@ -188,4 +167,4 @@ s_console (const char *format, ...)
     printf ("\n");
 }
 
-#endif  //  __ZHELPERS_H_INCLUDED__
+#endif // __ZHELPERS_H_INCLUDED__
