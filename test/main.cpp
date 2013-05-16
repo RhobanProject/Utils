11adc86ac0eb985c381512c0d97ae99eb54db1f3
Gregwar <g.passault@gmail.com>
2013-05-16 13:20:22 +0200

    Fixing tests

--- test/main.cpp
+++ test/main.cpp
@@ -16,13 +16,11 @@ int main()
 {
     TickMachine::get_tick_machine(); // FIXME: Without this, the test fails
 
-    /*
     runTests<TestCaseTest>("Testing test");
     runTests<ObserverTest>("Testing observer");
     runTests<CurveTest>("Testing curves");
     runTests<LoggingTest>("Testing logging");
     runTests<SocketsTest>("Testing sockets");
     runTests<TimedThreadTest>("Testing timed thread");
-    */
     runTests<ConfigFileTest>("Testing configfile");
 }
