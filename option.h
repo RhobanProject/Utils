70277512ba89658f03dd41843bd3447456acc388
olivier <olivier@ELEA-LINUX.(none)>
2013-06-05 23:46:05 +0200

    add ml like option type

---
+++ option.h
@@ -0,0 +1,54 @@
+/*****************************************************************************/
+/*! \file    option.hh
+ *  \author  AV
+ *  \date    2010-03
+ *
+ *  \brief   A simple template to mimic ML's option/None/Some
+ *****************************************************************************/
+
+#ifndef OPTION_HH
+#define OPTION_HH
+
+class OptionNoValueExc {};
+
+template <typename T>
+class Option
+{
+  T stored;
+  bool isSome;
+
+public:
+  /*! \brief Default constructor, similar to None in ML */
+  Option()
+  {
+    isSome = false;
+  }
+  /*! \brief Constructor with a value, similar to Some in ML */
+  Option(T val)
+  {
+    stored = val;
+    isSome = true;
+  }
+
+  /*! \brief Copy constructor */
+  Option(const Option &other) :
+    stored(other.stored),
+    isSome(other.isSome)
+  {};
+
+  /*! \brief Check if a value is stored */
+  bool hasValue()
+  {
+    return isSome;
+  }
+
+  /*! \brief Get the value if present, throw a dirty exception otherwise */
+  T getValue()
+  {
+    if (!isSome)
+      throw OptionNoValueExc();
+    return stored;
+  }
+};
+
+#endif /* OPTION_HH */
