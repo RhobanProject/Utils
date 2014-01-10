dba803d9d334e54826528bf5c1409c20abcae76a
Numero7 <hugo.gimbert@gmail.com>
2014-01-10 11:08:50 +0100

    removed binary files added zhelpers2.2.h and HighResclock

---
+++ timing/HighResClock.cpp
@@ -0,0 +1,22 @@
+#ifdef WIN32
+
+#include <HighResClock.h>
+
+namespace
+{
+	const long long g_Frequency = []() -> long long
+	{
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+		return frequency.QuadPart;
+	}();
+}
+
+HighResClock::time_point HighResClock::now()
+{
+	LARGE_INTEGER count;
+	QueryPerformanceCounter(&count);
+	return time_point(duration(count.QuadPart * static_cast<rep>(period::den) / g_Frequency));
+}
+
+#endif
