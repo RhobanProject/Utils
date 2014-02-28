2fb07446c7de1dde7a9550f5beb55b8b98dc9e87
Numero7 <root@debian>
2014-02-28 18:27:29 +0100

    sleep

--- timing/sleep.h
+++ timing/sleep.h
@@ -1,7 +1,7 @@
 #ifndef _TIMING_sys_wait_ms_H
 #define _TIMING_sys_wait_ms_H
 
-void ms_sys_wait_ms(long ms);
-void us_sys_wait_ms(long us);
+void ms_sleep(long ms);
+void us_sleep(long us);
 
 #endif
