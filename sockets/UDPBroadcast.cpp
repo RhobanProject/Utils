ce1a661fd1dcc0b06c78c0a6075603a7be441c10
medrimonia <medrimonia@gmail.com>
2014-11-03 11:22:02 +0100

    Welcome back udp broadcast

--- sockets/UDPBroadcast.cpp
+++ sockets/UDPBroadcast.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#ifdef DARWIN
+//#ifdef DARWIN
 #ifdef MSVC
 #else
 #include <sys/types.h>
@@ -199,4 +199,4 @@ void UDPBroadcast::retrieveBroadcastAddress()
 
 
 #endif
-#endif
+//#endif
