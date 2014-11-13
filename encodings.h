88e2b1038e4ea95f43ed85b7a43288a42166a69f
Numero7 <hugo.gimbert@gmail.com>
2014-11-13 01:42:17 +0100

    repaired encodings of floats and doubles

--- encodings.h
+++ encodings.h
@@ -1,18 +1,18 @@
 /*************************************************
- * Publicly released by Rhoban System, August 2012
- *             www.rhoban-system.fr
- *
- * Freely usable for non-commercial purposes
- *
- * Licence Creative Commons *CC BY-NC-SA
- * http://creativecommons.org/licenses/by-nc-sa/3.0
- *************************************************/
+* Publicly released by Rhoban System, August 2012
+*             www.rhoban-system.fr
+*
+* Freely usable for non-commercial purposes
+*
+* Licence Creative Commons *CC BY-NC-SA
+* http://creativecommons.org/licenses/by-nc-sa/3.0
+*************************************************/
 /*
- * encodings.h
- *
- *  Created on: 21 mars 2011
- *      Author: hugo
- */
+* encodings.h
+*
+*  Created on: 21 mars 2011
+*      Author: hugo
+*/
 
 #ifndef ENCODINGS_H_
 #define ENCODINGS_H_
@@ -56,10 +56,10 @@ inline void encode_int(int value, char * buf)
 	//faster solution but there may be big/small endian problems
 	//as well as alignment problems
 	//((int *) buf)[0] = value;
-	buf[0] = (char) ((value & 0xff000000) >>24);
-	buf[1] = (char) ((value & 0x00ff0000) >>16);
-	buf[2] = (char) ((value & 0x0000ff00) >>8);
-	buf[3] = (char) ((value & 0x000000ff) >>0);
+	buf[0] = (char)((value & 0xff000000) >> 24);
+	buf[1] = (char)((value & 0x00ff0000) >> 16);
+	buf[2] = (char)((value & 0x0000ff00) >> 8);
+	buf[3] = (char)((value & 0x000000ff) >> 0);
 }
 
 inline void encode_uint(ui32 value, char * buf)
@@ -67,10 +67,10 @@ inline void encode_uint(ui32 value, char * buf)
 	//faster solution but there may be big/small endian problems
 	//as well as alignment problems
 	//((ui32 *) buf)[0] = value;
-	buf[0] = (char) ((value & 0xff000000) >>24);
-	buf[1] = (char) ((value & 0x00ff0000) >>16);
-	buf[2] = (char) ((value & 0x0000ff00) >>8);
-	buf[3] = (char) ((value & 0x000000ff) >>0);
+	buf[0] = (char)((value & 0xff000000) >> 24);
+	buf[1] = (char)((value & 0x00ff0000) >> 16);
+	buf[2] = (char)((value & 0x0000ff00) >> 8);
+	buf[3] = (char)((value & 0x000000ff) >> 0);
 }
 
 inline void encode_short(short value, char * buf)
@@ -89,12 +89,13 @@ inline void encode_ushort(unsigned short value, char * buf)
 
 inline void encode_float(float value, char * buf)
 {
-	encode_int((int) (value * 1000), buf);
+	encode_uint(*(int *)&value, buf);
+	//	encode_int((int) (value * 1000), buf);
 }
 
 inline void encode_double(double value, char * buf)
 {
-	encode_int((int) (value * 1000), buf);
+	encode_float(value, buf);
 }
 /*!
 * todo: find a better encoding,
@@ -106,24 +107,24 @@ inline ui32 decode_uint(const char * buf)
 	//return ((ui32 *) buf)[0];
 	//cout << "Decoding int \t"<<((ui32 *) buf)[0]<< "\t\t("<<(int) buf[0]<<" "<< (int) buf[1]<<" "<< (int) buf[2]<<" "<< (int) buf[3]<<")"<<endl;
 
-	ui8* buff = (ui8*) buf;
-	ui32 a = buff[0]<<24;
-	ui32 b = buff[1]<<16;
-	ui32 c = buff[2]<<8;
+	ui8* buff = (ui8*)buf;
+	ui32 a = buff[0] << 24;
+	ui32 b = buff[1] << 16;
+	ui32 c = buff[2] << 8;
 	ui32 d = buff[3];
-	return a | b | c |d;
+	return a | b | c | d;
 
 	//	return  ((ui32)buf[0])<<24 | ((ui32)buf[1])<<16 | ((ui32)buf[2])<<8 | ((ui32)buf[3]);
 }
 
 inline int decode_int(const char * buf)
 {
-	ui8* buff = (ui8*) buf;
-	ui32 a = buff[0]<<24;
-	ui32 b = buff[1]<<16;
-	ui32 c = buff[2]<<8;
+	ui8* buff = (ui8*)buf;
+	ui32 a = buff[0] << 24;
+	ui32 b = buff[1] << 16;
+	ui32 c = buff[2] << 8;
 	ui32 d = buff[3];
-	return (int) (a | b | c |d);
+	return (int)(a | b | c | d);
 }
 
 inline unsigned short decode_ushort(const char * buf)
@@ -146,13 +147,14 @@ inline short decode_short(const char * buf)
 inline float decode_float(const char * buf)
 {
 	//return ldexp( decode_uint(buf+1) ,(int) buf[0]-128);
-	return decode_int(buf)/1000.0;
+	int val = decode_uint(buf);
+	return *(float *)&val;
 }
 
 inline string decode_string(const char * buf)
 {
 	ui32 length = decode_uint(buf);
-	return string(buf+4,length);
+	return string(buf + 4, length);
 }
 
 
