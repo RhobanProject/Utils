27e20120cc7cf741b1b8307d7bcb7649273e034f
Olivier <rhoban@Mowgly.(none)>
2015-03-17 15:27:43 +0100

    Fixing diffSec in TimeStamp

--- timing/TimeStamp.cpp
+++ timing/TimeStamp.cpp
@@ -30,8 +30,8 @@ namespace Utils {
 double diffSec(const Utils::Timing::TimeStamp & src,
                const Utils::Timing::TimeStamp & dst)
 {
-  int elapsedTicks = (dst - src).count();
-  return (double)(elapsedTicks) * steady_clock::period::num / steady_clock::period::den;
+  double elapsedTicks = (dst - src).count();
+  return elapsedTicks * steady_clock::period::num / steady_clock::period::den;
 }
 
 double diffMs(const Utils::Timing::TimeStamp & src,
